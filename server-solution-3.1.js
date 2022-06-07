6a26f50d15c65946fe547f19af410e4e525265b0
Victor Patru <patruvictor.mihai@gmail.com>
2022-06-08 00:09:23 +0300

    Add Chapter 3.1 solutions

---
+++ server-solution-3.1.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const PORT = 3001
+
+const persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Use /api/persons to receive the desired information</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}!`)
+})
